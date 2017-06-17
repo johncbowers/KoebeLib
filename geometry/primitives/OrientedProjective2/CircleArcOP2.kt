de144557b790ba842dc5201b25b360f2b57a8402
John Bowers <bowersjc@jmu.edu>
2017-06-17 14:09:07 -0400

    Added arc-arc intersections.

--- geometry/primitives/OrientedProjective2/CircleArcOP2.kt
+++ geometry/primitives/OrientedProjective2/CircleArcOP2.kt
@@ -18,15 +18,30 @@ class CircleArcOP2(val source: PointOP2, val target: PointOP2, val disk: DiskOP2
     // Euclidean radius of circle
     val radius: Double by lazy { disk.radius }
 
-    fun intersectWith(line: LineOP2): List<PointOP2> {
+    internal fun isBetweenSourceAndTarget(p: PointOP2): Boolean {
         //Signed area of triangle ABC = (1/2)( (B.x - A.x) * (C.y - A.y) - (C.x - A.x) * (B.y - A.y) );
-        return disk.intersectWith(line).filter {
-            p ->
-            val psx = p.hx * source.hw - source.hx * p.hw
-            val tsy = target.hy * source.hw - source.hy * target.hw
-            val tsx = target.hx * source.hw - source.hx * target.hw
-            val psy = p.hy * source.hw - source.hy * p.hw
-            0 < psx * tsy - tsx * psy
+        val psx = p.hx * source.hw - source.hx * p.hw
+        val tsy = target.hy * source.hw - source.hy * target.hw
+        val tsx = target.hx * source.hw - source.hx * target.hw
+        val psy = p.hy * source.hw - source.hy * p.hw
+        return 0 < psx * tsy - tsx * psy
+    }
+
+    fun intersectWith(aLine: LineOP2): List<PointOP2> {
+        return this.disk.intersectWith(aLine).filter {
+            p -> isBetweenSourceAndTarget(p)
+        }
+    }
+
+    fun intersectWith(aDisk: DiskOP2): List<PointOP2> {
+        return this.disk.intersectWith(aDisk).filter {
+            p -> isBetweenSourceAndTarget(p)
+        }
+    }
+
+    fun intersectWith(anArc: CircleArcOP2): List<PointOP2> {
+        return this.disk.intersectWith(anArc.disk).filter {
+            p -> isBetweenSourceAndTarget(p) && anArc.isBetweenSourceAndTarget(p)
         }
     }
 
